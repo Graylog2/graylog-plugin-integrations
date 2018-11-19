320276603cefedda3bc0633140c6b822aa14f3a2
Dan Torrey <danielowentorrey@gmail.com>
2018-11-19 13:12:54 -0600

    Add unit tests for message results before switching to templates
    Add unit test to verify the results of all message fields (TRAFFIC and SYSTEM only). This ensures that template functionality produces exactly the same results as the existing hard-coded mappings. Testing will be a snap once templates logic is implemented.

--- src/test/java/org.graylog.integrations/inputs/PANCodecTest.java
+++ src/test/java/org.graylog.integrations/inputs/PANCodecTest.java
@@ -5,6 +5,7 @@ import org.apache.commons.codec.binary.Hex;
 import org.graylog.integrations.inputs.paloalto.PaloAltoCodec;
 import org.graylog2.plugin.Message;
 import org.graylog2.plugin.journal.RawMessage;
+import org.joda.time.DateTime;
 import org.junit.Test;
 
 import java.io.FileWriter;
@@ -17,6 +18,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PANCodecTest {
 
@@ -31,12 +33,61 @@ public class PANCodecTest {
         PaloAltoCodec codec = new PaloAltoCodec(null);
         Message message = codec.decode(new RawMessage(SYSTEM_MESSAGE.getBytes()));
         assertEquals("SYSTEM", message.getField("pa_type"));
+        assertEquals(message.getField("module"), "general");
+        assertEquals(message.getField("description"), "\"Deviating device: Prod--2");
+        assertEquals(message.getField("serial_number"), "000710000506");
+        assertEquals(message.getField("source"), "Panorama-1");
+        assertEquals(message.getField("message"), "1,2018/09/19 11:50:35,000710000506,SYSTEM,general,0,2018/09/19 11:50:35,,general,,0,0,general,informational,\"Deviating device: Prod--2, Serial: 007255000045717, Object: N/A, Metric: mp-cpu, Value: 34\",1163103,0x0,0,0,0,0,,Panorama-1");
+        assertEquals(message.getField("pa_severity"), "informational");
+        assertEquals(message.getField("pa_time_generated"), "2018/09/19 11:50:35");
+        assertEquals(message.getField("event_id"), "general");
+        assertEquals(message.getField("pa_devicename"), "0");
+        assertEquals(message.getField("content_type"), "general");
+        assertEquals(message.getField("pa_virtualsys_name"), "0");
+        assertEquals(0, ((DateTime) message.getField("timestamp")).compareTo(new DateTime("2018-09-19T11:50:35.000-05:00")));
 
         message = codec.decode(new RawMessage(TRAFFIC_MESSAGE.getBytes()));
-        assertEquals("TRAFFIC", message.getField("pa_type"));
 
-        message = codec.decode(new RawMessage(THREAT_MESSAGE.getBytes()));
-        assertEquals("THREAT", message.getField("pa_type"));
+        assertEquals( message.getField("bytes_received"), 140L);
+        assertEquals( message.getField("source"), "Panorama--2");
+        assertEquals( message.getField("repeat_count"), 1L);
+        assertEquals( message.getField("pa_time_received"), "2018/09/19 11:50:32");
+        assertEquals( message.getField("interface_outbound"), "ethernet1/1");
+        assertEquals( message.getField("packets"), 6L);
+        assertEquals( message.getField("dest_location"), "10.20.30.40-10.20.30.40");
+        assertEquals( message.getField("src_ip"), "10.20.30.40");
+        assertEquals( message.getField("pa_time_generated"), "2018/09/19 11:50:32");
+        assertEquals( message.getField("protocol"), "tcp");
+        assertEquals( message.getField("content_type"), "end");
+        assertEquals( message.getField("packets_sent"), 4L);
+        assertEquals( message.getField("packets_received"), 2L);
+        assertEquals( message.getField("action"), "allow");
+        assertEquals( message.getField("pa_virtualsys_name"), "vsys1");
+        assertEquals( message.getField("dest_port"), 443L);
+        assertEquals( ((DateTime)message.getField("timestamp")).compareTo(new DateTime("2018-09-19T11:50:32.000-05:00")), 0);
+        assertEquals( message.getField("rule_name"), "HTTPS-strict");
+        assertEquals( message.getField("nat_src_ip"), "10.20.30.40");
+        assertEquals( message.getField("session_id"), 205742L);
+        assertEquals( message.getField("serial_number"), "007255000045717");
+        assertEquals(message.getField("message"), "1,2018/09/19 11:50:32,007255000045717,TRAFFIC,end,2049,2018/09/19 11:50:32,10.20.30.40,10.20.30.40,10.20.30.40,10.20.30.40,HTTPS-strict,,,incomplete,vsys1,Public,Public,ethernet1/1,ethernet1/1,ALK Logging,2018/09/19 11:50:32,205742,1,64575,443,41304,443,0x400070,tcp,allow,412,272,140,6,2018/09/19 11:50:15,0,any,0,54196730,0x8000000000000000,10.20.30.40-10.20.30.40,10.20.30.40-10.20.30.40,0,4,2,tcp-fin,13,16,0,0,,Prod--2,from-policy,,,0,,0,,N/A,0,0,0,0");
+        assertEquals( message.getField("bytes_sent"), 272L);
+        assertEquals( message.getField("dst_zone"), "Public");
+        assertEquals( message.getField("nat_src_port"), 41304L);
+        assertEquals( message.getField("src_port"), 64575L);
+        assertEquals( message.getField("src_location"), "10.20.30.40-10.20.30.40");
+        assertEquals( message.getField("log_action"), "ALK Logging");
+        assertEquals( message.getField("interface_inbound"), "ethernet1/1");
+        assertEquals( message.getField("application"), "incomplete");
+        assertEquals( message.getField("src_zone"), "Public");
+        assertEquals( message.getField("bytes"), 412L);
+        assertEquals( message.getField("dest_ip"), "10.20.30.40");
+        assertEquals( message.getField("pa_type"), "TRAFFIC");
+        assertEquals( message.getField("nat_dest_ip"), "10.20.30.40");
+        assertEquals( message.getField("category"), "any");
+        assertEquals( message.getField("nat_dest_port"), 443L);
+
+//        message = codec.decode(new RawMessage(THREAT_MESSAGE.getBytes()));
+//        assertEquals("THREAT", message.getField("pa_type"));
     }
 
     /**
