8933da4a2bdb6744a5399e1199de14112f345f48
Dan Torrey <danielowentorrey@gmail.com>
2018-11-20 15:04:46 -0600

    Remove remaining static mapping file

--- src/main/java/org/graylog/integrations/inputs/paloalto/types/MessageMapping.java
+++
@@ -1,9 +0,0 @@
-package org.graylog.integrations.inputs.paloalto.types;
-
-import com.google.common.collect.ImmutableMap;
-
-public interface MessageMapping {
-
-    ImmutableMap<Integer, FieldDescription> getMapping();
-
-}
