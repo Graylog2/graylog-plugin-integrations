b440ffbd4ecedeb8f6ea3231cb5504db0a1c716e
Dan Torrey <danielowentorrey@gmail.com>
2018-11-20 15:00:33 -0600

    Add tests for CSV template parsing

--- src/test/java/org.graylog.integrations/inputs/PANTemplateTest.java
+++ src/test/java/org.graylog.integrations/inputs/PANTemplateTest.java
@@ -1,9 +1,10 @@
 package org.graylog.integrations.inputs;
 
-import org.graylog.integrations.inputs.paloalto.types.PANTemplates;
 import org.graylog.integrations.inputs.paloalto.types.PANTemplateDefaults;
+import org.graylog.integrations.inputs.paloalto.types.PANTemplates;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -11,6 +12,8 @@ import static org.junit.Assert.assertNotNull;
  * Test parsing of raw PAN message templates.
  */
 public class PANTemplateTest {
+
+    public static final String DEFAULT_HEADER = "field,position,type";
 
     @Test
     public void parseTest() throws Exception {
@@ -41,6 +44,29 @@ public class PANTemplateTest {
             assertNotNull(v.getPosition());
             assertNotNull(v.getField());
             assertNotNull(v.getFieldType());
+        });
+    }
+
+    @Test
+    public void verifyCSVValidation() {
+
+        // Verify header checking.
+        PANTemplates templates = PANTemplates.newInstance("badheader",
+                                                          DEFAULT_HEADER,
+                                                          DEFAULT_HEADER);
+        assertEquals(3, templates.getAllErrors().size());
+        templates.getAllErrors().forEach(error -> {
+            assertTrue(error.contains("The header row is invalid"));
+        });
+
+        // Verify that invalid value messages returned for invalid values.
+        templates = PANTemplates.newInstance("field,position,type\n" +
+                                             "badvalue",
+                                             DEFAULT_HEADER,
+                                             DEFAULT_HEADER);
+
+        templates.getAllErrors().forEach(error -> {
+            assertTrue(error.contains("[] is not a valid"));
         });
     }
 }
