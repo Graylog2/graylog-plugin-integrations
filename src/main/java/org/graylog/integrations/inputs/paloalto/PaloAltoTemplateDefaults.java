3a44be48d308a7c30eef04e77623ef549cef669b
Dan Torrey <danielowentorrey@gmail.com>
2018-12-04 16:58:23 -0600

    Rename fields for improved compliance with GIM (Graylog Information Model)

--- src/main/java/org/graylog/integrations/inputs/paloalto/PaloAltoTemplateDefaults.java
+++ src/main/java/org/graylog/integrations/inputs/paloalto/PaloAltoTemplateDefaults.java
@@ -19,6 +19,13 @@ public class PaloAltoTemplateDefaults {
 
     }
 
+    /**
+     * GIM rename mappings (from Palo Alto docs to Graylog field names):
+     * source -> src
+     * address -> addr
+     * destination -> dest
+     */
+
     public static String SYSTEM_TEMPLATE = headerRow() +
                                            "1,receive_time,STRING\n" +
                                            "2,serial_number,STRING\n" +
@@ -54,17 +61,17 @@ public class PaloAltoTemplateDefaults {
                                            "4,threat_content_type,STRING\n" +
                                            "5,future_use1,STRING\n" +
                                            "6,generated_time,STRING\n" +
-                                           "7,src_ip,STRING\n" +
-                                           "8,dest_ip,STRING\n" +
-                                           "9,nat_src_ip,STRING\n" +
-                                           "10,nat_dest_ip,STRING\n" +
+                                           "7,src_addr,STRING\n" +
+                                           "8,dest_addr,STRING\n" +
+                                           "9,nat_src_addr,STRING\n" +
+                                           "10,nat_dest_addr,STRING\n" +
                                            "11,rule_name,STRING\n" +
-                                           "12,source_user,STRING\n" +
-                                           "13,destination_user,STRING\n" +
+                                           "12,src_user,STRING\n" +
+                                           "13,dest_user,STRING\n" +
                                            "14,application,STRING\n" +
                                            "15,virtual_system,STRING\n" +
-                                           "16,source_zone,STRING\n" +
-                                           "17,destination_zone,STRING\n" +
+                                           "16,src_zone,STRING\n" +
+                                           "17,dest_zone,STRING\n" +
                                            "18,inbound_interface,STRING\n" +
                                            "19,outbound_interface,STRING\n" +
                                            "20,log_action,STRING\n" +
@@ -85,8 +92,8 @@ public class PaloAltoTemplateDefaults {
                                            "35,direction,STRING\n" +
                                            "36,sequence_number,STRING\n" +
                                            "37,action_flags,STRING\n" +
-                                           "38,source_location,STRING\n" +
-                                           "39,destination_location,STRING\n" +
+                                           "38,src_location,STRING\n" +
+                                           "39,dest_location,STRING\n" +
                                            "40,future_use3,STRING\n" +
                                            "41,content_type,STRING\n" +
                                            "42,pcap_id,STRING\n" +
@@ -108,8 +115,8 @@ public class PaloAltoTemplateDefaults {
                                            "58,virtual_system_name,STRING\n" +
                                            "59,device_name,STRING\n" +
                                            "60,future_use4,STRING\n" +
-                                           "61,source_vm_uuid,STRING\n" +
-                                           "62,destination_vm_uuid,STRING\n" +
+                                           "61,src_vm_uuid,STRING\n" +
+                                           "62,dest_vm_uuid,STRING\n" +
                                            "63,http_method,STRING\n" +
                                            "64,tunnel_id_imsi,STRING\n" +
                                            "65,monitor_tag_imei,STRING\n" +
@@ -130,27 +137,27 @@ public class PaloAltoTemplateDefaults {
                                             "4,threat_content_type,STRING\n" +
                                             "5,future_use1,STRING\n" +
                                             "6,generated_time,STRING\n" +
-                                            "7,src_ip,STRING\n" +
-                                            "8,dest_ip,STRING\n" +
-                                            "9,nat_src_ip,STRING\n" +
-                                            "10,nat_dest_ip,STRING\n" +
+                                            "7,src_addr,STRING\n" +
+                                            "8,dest_addr,STRING\n" +
+                                            "9,nat_src_addr,STRING\n" +
+                                            "10,nat_dest_addr,STRING\n" +
                                             "11,rule_name,STRING\n" +
-                                            "12,source_user,STRING\n" +
-                                            "13,destination_user,STRING\n" +
+                                            "12,src_user,STRING\n" +
+                                            "13,dest_user,STRING\n" +
                                             "14,application,STRING\n" +
                                             "15,virtual_system,STRING\n" +
-                                            "16,source_zone,STRING\n" +
-                                            "17,destination_zone,STRING\n" +
+                                            "16,src_zone,STRING\n" +
+                                            "17,dest_zone,STRING\n" +
                                             "18,inbound_interface,STRING\n" +
                                             "19,outbound_interface,STRING\n" +
                                             "20,log_action,STRING\n" +
                                             "21,future_use2,STRING\n" +
                                             "22,session_id,LONG\n" +
                                             "23,repeat_count,LONG\n" +
-                                            "24,source_port,LONG\n" +
-                                            "25,destination_port,LONG\n" +
-                                            "26,nat_source_port,LONG\n" +
-                                            "27,nat_destination_port,LONG\n" +
+                                            "24,src_port,LONG\n" +
+                                            "25,dest_port,LONG\n" +
+                                            "26,nat_src_port,LONG\n" +
+                                            "27,nat_dest_port,LONG\n" +
                                             "28,flags,STRING\n" +
                                             "29,protocol,STRING\n" +
                                             "30,action,STRING\n" +
@@ -164,8 +171,8 @@ public class PaloAltoTemplateDefaults {
                                             "38,future_use3,STRING\n" +
                                             "39,sequence_number,STRING\n" +
                                             "40,action_flags,STRING\n" +
-                                            "41,source_location,STRING\n" +
-                                            "42,destination_location,STRING\n" +
+                                            "41,src_location,STRING\n" +
+                                            "42,dest_location,STRING\n" +
                                             "43,future_use4,STRING\n" +
                                             "44,packets_sent,LONG\n" +
                                             "45,packets_received,LONG\n" +
@@ -176,9 +183,9 @@ public class PaloAltoTemplateDefaults {
                                             "50,device_group_hierarchy_l4,STRING\n" +
                                             "51,virtual_system_name,STRING\n" +
                                             "52,device_name,STRING\n" +
-                                            "53,action_source,STRING\n" +
-                                            "54,source_vm_uuid,STRING\n" +
-                                            "55,destination_vm_uuid,STRING\n" +
+                                            "53,action_src,STRING\n" +
+                                            "54,src_vm_uuid,STRING\n" +
+                                            "55,dest_vm_uuid,STRING\n" +
                                             "56,tunnel_id_imsi,STRING\n" +
                                             "57,monitor_tag_imei,STRING\n" +
                                             "58,parent_session_id,STRING\n" +
