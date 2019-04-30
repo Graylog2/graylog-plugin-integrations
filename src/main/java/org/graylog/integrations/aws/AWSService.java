c4ea0cabb761ff0728b1a3115781e8c56e0ea498
Dan Torrey <dan@graylog.com>
2019-04-30 16:50:37 -0500

    Add aws package and service class, also fix broken commons-codec import
    Explicitly include commons-codec in this POM, since the AWS SDK v2 internally remaps commons-codec to another internal package. This makes the commons-codec from graylog-project-parent unavailable.
    See https://aws.amazon.com/blogs/developer/java-sdk-bundle

---
+++ src/main/java/org/graylog/integrations/aws/AWSService.java
@@ -0,0 +1,7 @@
+package org.graylog.integrations.aws;
+
+/**
+ * Service class that handles all general AWS service operations and business logic.
+ */
+public class AWSService {
+}
---
+++ src/test/java/org.graylog.integrations/aws/AWSServiceTest.java
@@ -0,0 +1,15 @@
+package org.graylog.integrations.aws;
+
+import org.junit.Test;
+
+/**
+ * Unit test for the AWSService class.
+ */
+public class AWSServiceTest {
+
+
+    @Test
+    public void name() {
+
+    }
+}
