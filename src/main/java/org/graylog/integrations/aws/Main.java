0c1e05049d51dc512abc31304703acc143145197
Claudia Ortiz <claudia@graylog.com>
2019-06-11 17:43:53 -0500

    Update temporary Main class

--- src/main/java/org/graylog/integrations/aws/Main.java
+++ src/main/java/org/graylog/integrations/aws/Main.java
@@ -3,14 +3,14 @@ package org.graylog.integrations.aws;
 import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.kinesis.KinesisClient;
+import software.amazon.awssdk.services.kinesis.model.GetRecordsRequest;
+import software.amazon.awssdk.services.kinesis.model.GetRecordsResponse;
 import software.amazon.awssdk.services.kinesis.model.GetShardIteratorRequest;
 import software.amazon.awssdk.services.kinesis.model.ListShardsRequest;
 import software.amazon.awssdk.services.kinesis.model.ListShardsResponse;
 import software.amazon.awssdk.services.kinesis.model.PutRecordsRequest;
 import software.amazon.awssdk.services.kinesis.model.PutRecordsRequestEntry;
 import software.amazon.awssdk.services.kinesis.model.ShardIteratorType;
-import software.amazon.awssdk.services.kinesis.model.StartingPosition;
-import software.amazon.awssdk.services.kinesis.model.SubscribeToShardRequest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,44 +22,64 @@ public class Main {
         String kinesisStream = "integrations-test-kinesis-stream";
         KinesisClient kinesisClient = KinesisClient.builder().region(Region.US_EAST_1).build();
 
-        // Add in arbitrary data
+        // Add in arbitrary records
         byte[] bytes = "Around the world, Around the world.".getBytes();
 
-
+        //ADD RECORDS IN
+        // Create an arbitrary collection of records
         List<PutRecordsRequestEntry> putRecordsRequestEntryList = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 50; i++) {
             PutRecordsRequestEntry putRecordsRequestEntry = PutRecordsRequestEntry.builder()
                     .data(SdkBytes.fromByteArray(bytes))
                     .partitionKey(String.format("partitionKey-%d", i))
                     .build();
             putRecordsRequestEntryList.add(putRecordsRequestEntry);
         }
-        PutRecordsRequest putRecordsRequest = PutRecordsRequest.builder().streamName(kinesisStream).records(putRecordsRequestEntryList).build();
-        kinesisClient.putRecords(putRecordsRequest);
-        System.out.println(putRecordsRequest);
-
-        // Find shards that exist (there may be more than 1) and acquire shardId
-        // Assume only one exists for now
-        ListShardsRequest listShardsRequest = ListShardsRequest.builder().streamName(kinesisStream).build();
-
-        GetShardIteratorRequest getShardIteratorRequest = GetShardIteratorRequest.builder()
+        // Create a PutRecordRequest and pass to the Kinesis client
+        PutRecordsRequest putRecordsRequest = PutRecordsRequest.builder()
                 .streamName(kinesisStream)
-                .shardIteratorType(ShardIteratorType.TRIM_HORIZON)
+                .records(putRecordsRequestEntryList)
                 .build();
+        // Kinesis client adds collection to kinesis stream
+        kinesisClient.putRecords(putRecordsRequest);
 
+        //KinesisService.retrieveKinesisLogs(kinesisStream);
+
+        // TAKE RECORDS OUT
+        // Create ListShard request and response and designate the Kinesis stream
+        ListShardsRequest listShardsRequest = ListShardsRequest.builder().streamName(kinesisStream).build();
         ListShardsResponse listShardsResponse = kinesisClient.listShards(listShardsRequest);
-        SubscribeToShardRequest subscribeToShardRequest = SubscribeToShardRequest.builder()
-                .startingPosition(StartingPosition.builder().type(ShardIteratorType.TRIM_HORIZON).build())
-                .build();
 
-        System.out.print("listShardsRequest:  " + listShardsRequest);
+        String shardID = "";
+        int shardNum = listShardsResponse.shards().size();
 
+        // Iterate through the shards that exist
+        String nextShardIterator = "";
+        for (int i = 0; i < shardNum; i++) {
+            shardID = listShardsResponse.shards().get(i).shardId();
+            System.out.println("Shard Id: " + shardID);
+            GetShardIteratorRequest getShardIteratorRequest = GetShardIteratorRequest.builder()
+                    .shardId(shardID)
+                    .streamName(kinesisStream)
+                    .shardIteratorType(ShardIteratorType.TRIM_HORIZON)
+                    .build();
+            String shardIterator = kinesisClient.getShardIterator(getShardIteratorRequest).shardIterator();
 
-        // Get the shardIterator and start with the oldest record in the stream
-        //GetShardIteratorRequest getShardIteratorRequest = GetShardIteratorRequest.builder().shardIteratorType(ShardIteratorType.TRIM_HORIZON).build();
-        // Create shardIterator to find shards
-        // Call getRecord in a loop (use GetShardIterator)
+            System.out.println("Shard Interator: " + shardIterator);
+            GetRecordsRequest getRecordsRequest = GetRecordsRequest.builder().shardIterator(shardIterator).build();
+            GetRecordsResponse getRecordsResponse = kinesisClient.getRecords(getRecordsRequest);
+            // TODO iterate through all the shardIterators to account for all the records
+//            nextShardIterator = getRecordsResponse.nextShardIterator();
+//            GetRecordsRequest getRecordsRequest = GetRecordsRequest.builder().shardIterator(nextShardIterator).build();
+//            GetRecordsResponse getRecordsResponse = kinesisClient.getRecords(getRecordsRequest);
+//            System.out.println(getRecordsResponse.records().size());
+//            nextShardIterator = getRecordsResponse.nextShardIterator();
 
-
+            int recordSize = getRecordsResponse.records().size();
+            System.out.println("Record size: " + recordSize);
+            for (int j = 0; j < recordSize; j++) {
+                System.out.println("[" + j + "]" + new String(getRecordsResponse.records().get(j).data().asByteArray()));
+            }
+        }
     }
 }
