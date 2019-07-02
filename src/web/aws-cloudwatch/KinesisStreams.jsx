0e74936d0b720796502dc74c0000fc10ebf46983
Kyle Knight <kyle@graylog.com>
2019-07-02 11:43:23 -0500

    Fixing up rebase artifacts from squash of irrelevant commits

--- src/web/aws-cloudwatch/KinesisStreams.jsx
+++ src/web/aws-cloudwatch/KinesisStreams.jsx
@@ -4,11 +4,7 @@ import { Button, Col, Row } from 'react-bootstrap';
 
 import { Input } from 'components/bootstrap';
 import FormAdvancedOptions from './FormAdvancedOptions';
-<<<<<<< HEAD
 import Routes from '../common/Routes';
-=======
-import ROUTES from '../common/Routes';
->>>>>>> f7268b3... Fixing conflicts
 
 const KinesisStreams = ({ onChange, onSubmit, values, toggleAdvancedOptions, visibleAdvancedOptions }) => {
   return (
@@ -16,11 +12,7 @@ const KinesisStreams = ({ onChange, onSubmit, values, toggleAdvancedOptions, vis
       <Col md={8}>
         <form onSubmit={onSubmit}>
           <h2>Choose Kinesis Stream</h2>
-<<<<<<< HEAD
           <p>Below is a list of all Kinesis Streams found within the specified AWS account. Please choose the Stream you would like us to read messages from, or follow the directions to begin <a href={Routes.INTEGRATIONS.AWS.CLOUDWATCH.step('kinesis-setup')}>setting up your CloudWatch Log Group</a> to feed messages into a new Kinesis Stream.</p>
-=======
-          <p>Below is a list of all the Streams we found configured within Kinesis. Please choose the Stream you would like us to parse, or follow the directions to begin <a href={ROUTES.INTEGRATIONS.CLOUDWATCH_STEP('kinesis-setup')}>setting up your CloudWatch Group</a> to feed into a new Kinesis Stream.</p>
->>>>>>> f7268b3... Fixing conflicts
 
           <Input id="awsCloudWatchKinesisStream"
                  type="select"
