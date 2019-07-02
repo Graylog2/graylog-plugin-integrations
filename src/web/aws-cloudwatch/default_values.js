f6a15cd299b61b1291ebe2845b888b20d02cf6ed
Kyle Knight <kyle@graylog.com>
2019-07-02 10:36:29 -0500

    default_values.js files

---
+++ src/web/aws-cloudwatch/default_values.js
@@ -0,0 +1,9 @@
+const DEFAULT_VALUES = { /* Default Advanced Values */
+  awsCloudWatchGlobalInput: '',
+  awsCloudWatchAssumeARN: '',
+  awsCloudWatchBatchSize: '10000',
+  awsCloudWatchThrottleEnabled: '',
+  awsCloudWatchThrottleWait: '1000',
+};
+
+export default DEFAULT_VALUES;
