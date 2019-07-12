3c83f25351edb3c754684012d695f49bbe9371da
Kyle Knight <kyle@graylog.com>
2019-07-12 16:14:20 -0500

    Adding all the files

---
+++ src/web/aws-cloudwatch/hooks/fetchReducer.js
@@ -0,0 +1,23 @@
+const fetchReducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return { ...state, isLoading: true, isError: false };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      throw new Error();
+  }
+};
+
+export default fetchReducer;
