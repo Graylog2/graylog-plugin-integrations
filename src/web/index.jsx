84d1c7ec4f896b1ae8c5fd33f0ff2d71462f3cd6
Kyle Knight <kyle@graylog.com>
2019-07-18 10:42:03 -0500

    custom component

--- src/web/index.jsx
+++ src/web/index.jsx
@@ -5,6 +5,7 @@ import { PluginManifest, PluginStore } from 'graylog-web-plugin/plugin';
 import Routes from 'common/Routes.js';
 
 import AWSServices from './aws/Services';
+import InputConfiguration from './aws/InputConfiguration';
 import AWSCloudWatchApp from './aws-cloudwatch/CloudWatchApp';
 import packageJson from '../../package.json';
 
@@ -12,12 +13,12 @@ const manifest = new PluginManifest(packageJson, {
   routes: [
     { path: Routes.INTEGRATIONS.AWS.SERVICES, component: AWSServices },
     { path: Routes.INTEGRATIONS.AWS.CLOUDWATCH.index, component: AWSCloudWatchApp },
-    { path: Routes.INTEGRATIONS.AWS.CLOUDWATCH.step(':step'), component: AWSCloudWatchApp },
+    { path: Routes.INTEGRATIONS.AWS.CLOUDWATCH.index, component: AWSCloudWatchApp },
   ],
   inputConfiguration: [
     {
-      matches: type => /^org.graylog.integrations.aws/.test(type),
-      path: Routes.INTEGRATIONS.AWS.CLOUDWATCH.index,
+      type: 'org.graylog.integrations.aws.inputs.AWSInput',
+      component: InputConfiguration,
     },
   ],
 });
