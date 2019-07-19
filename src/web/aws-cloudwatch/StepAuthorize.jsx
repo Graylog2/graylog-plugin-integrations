744557a9e301be8723233fa7af792ee5f08074cc
Kyle Knight <kyle@graylog.com>
2019-07-19 07:40:11 -0500

    lint

--- src/web/aws-cloudwatch/StepAuthorize.jsx
+++ src/web/aws-cloudwatch/StepAuthorize.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Col, Row } from 'react-bootstrap';
 import styled from '@emotion/styled';
