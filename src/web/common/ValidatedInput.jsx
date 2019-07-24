57b38a574d57a7638ce0b6b9a55e46476733d609
Kyle Knight <kyle@graylog.com>
2019-07-23 19:09:42 -0500

    import styled-components

--- src/web/common/ValidatedInput.jsx
+++ src/web/common/ValidatedInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
+import styled from 'styled-components';
 
 import { Input } from 'components/bootstrap';
 
