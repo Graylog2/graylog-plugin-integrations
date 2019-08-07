0cfd9a552ffd0be2369fc30e0290238ecd513dd8
Dan Torrey <dan@graylog.com>
2019-08-07 11:54:51 -0500

    Add mocking for UI testing
    Also remove full setup, since the individual steps will be used.

--- src/main/java/org/graylog/integrations/aws/resources/KinesisSetupResource.java
+++ src/main/java/org/graylog/integrations/aws/resources/KinesisSetupResource.java
@@ -9,20 +9,15 @@ import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.graylog.integrations.aws.AWSPermissions;
 import org.graylog.integrations.aws.resources.requests.CreateLogSubscriptionRequest;
 import org.graylog.integrations.aws.resources.requests.CreateRolePermissionRequest;
-import org.graylog.integrations.aws.resources.requests.KinesisFullSetupRequest;
 import org.graylog.integrations.aws.resources.requests.KinesisNewStreamRequest;
 import org.graylog.integrations.aws.resources.responses.CreateLogSubscriptionResponse;
 import org.graylog.integrations.aws.resources.responses.CreateRolePermissionResponse;
-import org.graylog.integrations.aws.resources.responses.KinesisFullSetupResponse;
-import org.graylog.integrations.aws.resources.responses.KinesisFullSetupResponseStep;
 import org.graylog.integrations.aws.resources.responses.KinesisNewStreamResponse;
-import org.graylog.integrations.aws.service.AWSService;
 import org.graylog.integrations.aws.service.CloudWatchService;
 import org.graylog.integrations.aws.service.KinesisService;
 import org.graylog2.plugin.rest.PluginRestResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
 
 import javax.inject.Inject;
 import javax.validation.Valid;
@@ -32,7 +27,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
 
 /**
  * Web endpoints for the Kinesis auto-setup.
@@ -49,136 +43,65 @@ public class KinesisSetupResource implements PluginRestResource {
     private KinesisService kinesisService;
     private CloudWatchService cloudWatchService;
 
-    // Guice
+    // Enable mocked responses for UI testing.
+    // TODO: Remove later.
+    private boolean enableMockResponses = true;
+
     @Inject
     public KinesisSetupResource(KinesisService kinesisService, CloudWatchService cloudWatchService) {
         this.kinesisService = kinesisService;
         this.cloudWatchService = cloudWatchService;
     }
 
-    /**
-     * 1.  Create a new Kinesis stream and check that its active
-     * INPUT: credentials and streamName acquire from the user
-     * OUTPUT: String streamArn
-     * BACKEND DETAILS:
-     * void createKinesisStream(KinesisClient kinesisClient, String stream)
-     * StreamDescription streamDescription = checkKinesisStreamStatus(kinesisClient, stream);
-     * String streamArn = streamDescription.streamARN();
-     */
     @POST
     @Timed
     @Path("/create_stream")
     @ApiOperation(value = "Step 1: Attempt to create a new kinesis stream and wait for it to be ready.")
     @RequiresPermissions(AWSPermissions.AWS_READ)
     public KinesisNewStreamResponse createNewKinesisStream(@ApiParam(name = "JSON body", required = true) @Valid @NotNull
-                                                                   KinesisNewStreamRequest request) {
+                                                                   KinesisNewStreamRequest request) throws InterruptedException {
 
         LOG.info("Request: [{}]", request);
-        // Mock response
-        //return KinesisNewStreamResponse.create(request.streamName(), "a-fake-arn", "The stream is good-to-go");
+        if (enableMockResponses) {
+            Thread.sleep(1000);
+            return KinesisNewStreamResponse.create(request.streamName(), "a-fake-arn", "The stream is good-to-go");
+        }
 
         // Real method call is already implemented. Commented out for now to allow UI to be mocked out easier.
         return kinesisService.createNewKinesisStream(request);
     }
 
-    /**
-     * 2.  Add role with required permissions and acquire roleArn
-     * INPUT: roleName acquired from the user
-     * OUTPUT: streamARN acquired from step 3
-     * BACKEND DETAILS:
-     * void setRolePermissions(iam, roleName, streamArn, region.toString());
-     * String roleArn = getNewRolePermissions(iam, roleName);
-     */
     @POST
     @Timed
     @Path("/create_subscription_policy")
     @ApiOperation(value = "Step 2: Create AWS IAM policy needed for CloudWatch to write logs to Kinesis")
     @RequiresPermissions(AWSPermissions.AWS_READ)
     public CreateRolePermissionResponse createPolicies(@ApiParam(name = "JSON body", required = true) @Valid @NotNull
-                                                               CreateRolePermissionRequest request) {
+                                                               CreateRolePermissionRequest request) throws InterruptedException {
         LOG.info("Request: [{}]", request);
 
-        // Mock response
-        //return CreateLogSubscriptionPolicyResponse.create("fake-policy-name", "fake-policy-arn");
+        if (enableMockResponses) {
+            Thread.sleep(1000);
+            return CreateRolePermissionResponse.create("Policy created successfykky", "fake-policy-arn");
+        }
+
         return kinesisService.autoKinesisPermissions(request);
     }
 
-    /**
-     * Creates a {@see <a href="https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/SubscriptionFilters.html">Cloud Watch Subscription Filter</a>},
-     * which subscribes a kinesis stream to a CloudWatch log group. This will cause batches of CloudWatch loge messages
-     * to be put in the Kinesis stream in a GZipped JSON byte array payload.
-     * <p>
-     * 3.  Add the subscription to the Cloudwatch log group for kinesis
-     * INPUT: credentials, logGroup, filterName, filterPattern acquired from user
-     * OUTPUT: streamArn acquired from step 3
-     * BACKEND DETAILS:
-     * roleArn acquired from step 4
-     * void addSubscriptionFilter(String logGroup, CloudWatchLogsClient cloudWatch, String streamArn, String roleArn, String filterName, String filterPattern)
-     */
     @POST
     @Timed
     @Path("/create_subscription")
     @ApiOperation(value = "Step 3: Subscribe a Kinesis stream to a CloudWatch log group")
     @RequiresPermissions(AWSPermissions.AWS_READ)
     public CreateLogSubscriptionResponse createSubscription(@ApiParam(name = "JSON body", required = true) @Valid @NotNull
-                                                                    CreateLogSubscriptionRequest request) {
+                                                                    CreateLogSubscriptionRequest request) throws InterruptedException {
         LOG.info("Request: [{}]", request);
 
-        // TODO: We'll need to give some thought to how to effectively build the UI for the filterPattern and filterName
-        //   Perhaps we can provide default initialized values (eg. " " for filterPattern [matches all], and some generic pattern name).
+        if (enableMockResponses) {
+            Thread.sleep(1000);
+            return CreateLogSubscriptionResponse.create("Subscription created successfully");
+        }
 
-        // Mock response
-        //return CreateLogSubscriptionResponse.create("Subscription created successfully");
         return cloudWatchService.addSubscriptionFilter(request);
-    }
-
-    /**
-     * Full Kinesis setup.
-     *
-     * @param request
-     * @return KinesisFullSetupResponse
-     */
-    @POST
-    @Timed
-    @Path("/full_setup")
-    @ApiOperation(value = "Full setup: Get all available AWS CloudWatch log groups names for the specified region")
-    @RequiresPermissions(AWSPermissions.AWS_READ)
-    public KinesisFullSetupResponse kinesisFullSetup(@ApiParam(name = "JSON body", required = true) @Valid @NotNull
-                                                             KinesisFullSetupRequest request) {
-        LOG.info("Request: [{}]", request);
-        final ArrayList<KinesisFullSetupResponseStep> setupSteps = new ArrayList<>();
-
-        // Step 1
-        KinesisNewStreamResponse kinesisNewStreamResponse = kinesisService.createNewKinesisStream(KinesisNewStreamRequest.create(request.region(),
-                                                                                                                                 request.awsAccessKeyId(),
-                                                                                                                                 request.awsSecretAccessKey(),
-                                                                                                                                 request.streamName()));
-        String streamArn = kinesisNewStreamResponse.streamArn();
-        setupSteps.add(KinesisFullSetupResponseStep.create(true, "Create Stream", kinesisNewStreamResponse.explanation()));
-
-        // Step 2
-        CreateRolePermissionResponse response = kinesisService.autoKinesisPermissions(CreateRolePermissionRequest.create(request.region(),
-                                                                                                                         request.awsAccessKeyId(),
-                                                                                                                         request.awsSecretAccessKey(),
-                                                                                                                         request.streamName(),
-                                                                                                                         request.roleName(),
-                                                                                                                         request.rolePolicyName()));
-        String roleArn = response.roleArn();
-        setupSteps.add(KinesisFullSetupResponseStep.create(true, "Create Policy with role Arn", response.explanation()));
-
-        // Step 3
-        // TODO optimize this call, it needs a cloudwatchLogClient to call addSubscription
-        CreateLogSubscriptionRequest logSubscriptionRequest = CreateLogSubscriptionRequest.create(request.region(),
-                                                                                                  request.awsAccessKeyId(),
-                                                                                                  request.awsSecretAccessKey(),
-                                                                                                  request.getLogGroupName(),
-                                                                                                  request.filterName(),
-                                                                                                  request.filterPattern(),
-                                                                                                  streamArn,
-                                                                                                  roleArn);
-        CreateLogSubscriptionResponse logSubscriptionResponse = cloudWatchService.addSubscriptionFilter(logSubscriptionRequest);
-
-        setupSteps.add(KinesisFullSetupResponseStep.create(false, "Subscribe stream to group", logSubscriptionResponse.explanation()));
-        return KinesisFullSetupResponse.create(false, "Auto-setup was not fully successful!", setupSteps);
     }
 }
