9877367258f77ce0e6268c4c559b214d36ac7919
Kyle Knight <kyle@graylog.com>
2019-08-13 13:18:26 -0500

    Agree TOS in Modal

--- src/web/aws-cloudwatch/auto-setup-steps/KinesisSetupSteps.jsx
+++ src/web/aws-cloudwatch/auto-setup-steps/KinesisSetupSteps.jsx
@@ -1,14 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { Alert, Panel } from 'react-bootstrap';
 
 import URLUtils from 'util/URLUtils';
 import fetch from 'logic/rest/FetchProvider';
 
-import { ApiRoutes } from '../common/Routes';
+import { ApiRoutes } from '../../common/Routes';
 
-import { FormDataContext } from './context/FormData';
-import { awsAuth } from './context/default_settings';
+import { FormDataContext } from '../context/FormData';
+import { awsAuth } from '../context/default_settings';
 import KinesisSetupStep from './KinesisSetupStep';
 
 const parseError = (error) => {
@@ -17,7 +16,7 @@ const parseError = (error) => {
   return fullError || error.message;
 };
 
-const KinesisSetupSteps = ({ onSubmit }) => {
+const KinesisSetupSteps = ({ onSuccess, onError }) => {
   const { formData } = useContext(FormDataContext);
   const { key, secret } = awsAuth(formData);
   const { awsCloudWatchAwsRegion } = formData;
@@ -76,7 +75,6 @@ const KinesisSetupSteps = ({ onSubmit }) => {
   }
 
   // State for each step must be maintained separately in order for the UI to be correctly updated.
-  const [success, setSuccess] = useState(false);
   const [streamStep, setStreamStep] = useState({
     label: 'Create Kinesis Stream',
     route: ApiRoutes.INTEGRATIONS.AWS.KINESIS_AUTO_SETUP.CREATE_STREAM,
@@ -106,6 +104,7 @@ const KinesisSetupSteps = ({ onSubmit }) => {
           // eslint-disable-next-line
           console.log('Setup request error:', e);
           const error = errorState(parseError(e));
+          onError();
           setStep({ ...step, state: error });
           throw error;
         }
@@ -128,7 +127,7 @@ const KinesisSetupSteps = ({ onSubmit }) => {
         streamArn,
         response.role_arn));
 
-      setSuccess(true);
+      onSuccess();
     }
 
     // TODO: Display success message.
@@ -139,29 +138,17 @@ const KinesisSetupSteps = ({ onSubmit }) => {
   }, []);
 
   return (
-    <Panel header={<p>Executing Auto-Setup</p>}>
+    <>
       <KinesisSetupStep step={streamStep} />
       <KinesisSetupStep step={policyStep} />
       <KinesisSetupStep step={subscriptionStep} />
-
-      {success && (
-      <>
-        <Alert key="delayedLogs" variant="warning">
-          It may take up to ten minutes for the first messages to arrive in the Kinesis Stream. The Kinesis Health Check in the following step will not complete successfully until messages are present in the stream. Please see the official <a href="https://docs.aws.amazon.com/AmazonCloudWatch/latest/logs/Subscriptions.html" target="_blank" rel="noopener noreferrer">CloudWatch Subscriptions</a> documentation for more information.
-        </Alert>
-
-        <button onClick={onSubmit}
-                type="button"
-                className="btn btn-primary">Continue Setup
-        </button>
-      </>
-      )}
-    </Panel>
+    </>
   );
 };
 
 KinesisSetupSteps.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+  onSuccess: PropTypes.func.isRequired,
+  onError: PropTypes.func.isRequired,
 };
 
 export default KinesisSetupSteps;
