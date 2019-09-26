7bca631c07a63093d86f6b3f9a1ddf90546fe02b
Kyle Knight <kyle@graylog.com>
2019-09-26 10:43:46 -0500

    Adding Automatic content

--- src/web/aws/authentication/AWSAuthenticationTypes.jsx
+++ src/web/aws/authentication/AWSAuthenticationTypes.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
+import { Input } from 'components/bootstrap';
+
 import { FormDataContext } from 'aws/context/FormData';
 import KeySecret from './KeySecret';
 import ARN from './ARN';
@@ -33,11 +35,15 @@ const AWSAuthenticationTypes = ({ onChange }) => {
 
   return (
     <>
-      <select name="awsAuthType" id="awsAuthType" onChange={handleTypeChange}>
+      <Input type="select"
+             name="awsAuthType"
+             id="awsAuthType"
+             onChange={handleTypeChange}
+             label="AWS Authentication Type">
         <option value="automatic" selected={isType(TYPE_AUTOMATIC)}>Automatic</option>
         <option value="key-secret" selected={isType(TYPE_KEYSECRET)}>Key &amp; Secret</option>
         <option value="arn" selected={isType(TYPE_ARN)}>ARN - Amazon Resource Name</option>
-      </select>
+      </Input>
 
       {isType(TYPE_KEYSECRET) && (
       <KeySecret awsKey={formData.awsCloudWatchAwsKey}
--- src/web/aws/authentication/Automatic.jsx
+++ src/web/aws/authentication/Automatic.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { Table } from 'components/graylog';
 
 const Automatic = () => {
   return (
     <div>
-      <p>Automatic</p>
+      <StyledTable condensed>
+        <tbody>
+          <tr>
+            <th>Environment variables</th>
+            <td><code>AWS_ACCESS_KEY_ID</code> and <code>AWS_SECRET_ACCESS_KEY</code></td>
+          </tr>
+          <tr>
+            <th>Java system properties</th>
+            <td><code>aws.accessKeyId</code> and <code>aws.secretKey</code></td>
+          </tr>
+          <tr>
+            <th>Default credential profiles file</th>
+            <td>Typically located at <code>~/.aws/credentials</code></td>
+          </tr>
+          <tr>
+            <th>Amazon ECS container credentials</th>
+            <td>Loaded from the Amazon ECS if the environment variable <code>AWS_CONTAINER_CREDENTIALS_RELATIVE_URI</code> is set</td>
+          </tr>
+          <tr>
+            <th>Instance profile credentials</th>
+            <td>Used on EC2 instances, and delivered through the Amazon EC2 metadata service</td>
+          </tr>
+        </tbody>
+      </StyledTable>
     </div>
   );
 };
+
+const StyledTable = styled(Table)`
+  && {
+    max-width: 75%;
+    margin: 25px 0;
+  }
+`;
 
 export default Automatic;
