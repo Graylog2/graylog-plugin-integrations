7f8e4bf9a91fc49aece423429d922ac63056d756
Kyle Knight <kyle@graylog.com>
2019-09-26 10:21:50 -0500

    ARN field

--- src/web/aws/authentication/ARN.jsx
+++ src/web/aws/authentication/ARN.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ARN = () => {
+import { Input } from 'components/bootstrap';
+
+const ARN = ({ awsARN, onChange }) => {
   return (
-    <div>
-      <p>ARN</p>
-    </div>
+    <Input id="awsCloudWatchAssumeARN"
+           type="text"
+           value={awsARN}
+           onChange={onChange}
+           label="AWS Amazon Resource Name (ARN)"
+           help="Role ARN with required permissions (cross account access)" />
   );
+};
+
+ARN.propTypes = {
+  awsARN: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+};
+
+ARN.defaultProps = {
+  awsARN: '',
 };
 
 export default ARN;
