238b22a75b45ffba6d9e2d06ee88c719943d1491
Claudia Ortiz <claudia@graylog.com>
2020-02-26 11:04:21 -0600

    Modify OktaResponse

--- src/main/java/org/graylog/integrations/okta/OktaResponse.java
+++ src/main/java/org/graylog/integrations/okta/OktaResponse.java
@@ -5,25 +5,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.auto.value.AutoValue;
 import org.graylog.autovalue.WithBeanGetter;
 
-import java.util.List;
-
 @JsonAutoDetect
 @AutoValue
 @WithBeanGetter
 public abstract class OktaResponse {
 
     private static final String SYSTEM_LOGS = "systemLogs";
-    private static final String TOTAL = "total";
 
     @JsonProperty(SYSTEM_LOGS)
-    public abstract List<String> logs();
-
-    @JsonProperty(TOTAL)
-    public abstract long total();
+    public abstract String logs();
 
     // TODO auto create a okta system log event object https://developer.okta.com/docs/reference/api/system-log/#logevent-object
-    public static OktaResponse create(@JsonProperty(SYSTEM_LOGS) List<String> systemLogs,
-                                      @JsonProperty(TOTAL) long total) {
-        return new AutoValue_OktaResponse(systemLogs, total);
+    public static OktaResponse create(@JsonProperty(SYSTEM_LOGS) String systemLogs) {
+        return new AutoValue_OktaResponse(systemLogs);
     }
 }
