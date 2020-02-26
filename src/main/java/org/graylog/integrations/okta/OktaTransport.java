96fc109d7c2c1d449418b28e05eb492309b7c1dc
Claudia Ortiz <claudia@graylog.com>
2020-02-26 17:53:07 -0600

    Code clean up and add comment block

--- src/main/java/org/graylog/integrations/okta/OktaTransport.java
+++ src/main/java/org/graylog/integrations/okta/OktaTransport.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of Graylog.
+ *
+ * Graylog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Graylog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Graylog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.graylog.integrations.okta;
 
 import com.codahale.metrics.MetricSet;
@@ -12,21 +28,15 @@ import org.graylog2.plugin.inputs.annotations.FactoryClass;
 import org.graylog2.plugin.inputs.codecs.CodecAggregator;
 import org.graylog2.plugin.inputs.transports.ThrottleableTransport;
 import org.graylog2.plugin.inputs.transports.Transport;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 
 // TODO determine if ThrottleableTransport is the most suitable transport to extend/use
 public class OktaTransport extends ThrottleableTransport {
-    private static final Logger LOG = LoggerFactory.getLogger(OktaTransport.class);
     public static final String NAME = "okta-transport";
 
     private LocalMetricRegistry localRegistry;
     private final Configuration configuration;
-
-    private static final String CK_OKTA_DOMAIN = "okta_domain";
-    private static final String CK_OKTA_API_KEY = "okta_api_key";
 
 
     public static final int DEFAULT_BATCH_SIZE = 10000;
