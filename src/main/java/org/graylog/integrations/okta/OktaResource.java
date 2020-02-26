bc7db5b79e3b68a4202200d892304ad3cf055cdc
Claudia Ortiz <claudia@graylog.com>
2020-02-26 12:07:17 -0600

    Update syslogs to call getSystemLogs

--- src/main/java/org/graylog/integrations/okta/OktaResource.java
+++ src/main/java/org/graylog/integrations/okta/OktaResource.java
@@ -3,13 +3,9 @@ package org.graylog.integrations.okta;
 import com.codahale.metrics.annotation.Timed;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import org.graylog2.plugin.rest.PluginRestResource;
 import org.graylog2.rest.resources.system.inputs.AbstractInputsResource;
 import org.graylog2.shared.inputs.MessageInputFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -17,7 +13,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import java.io.IOException;
 
 @Api(value = "Okta", description = "Okta Integrations")
 @Path("/okta")
@@ -27,12 +22,8 @@ public class OktaResource extends AbstractInputsResource implements PluginRestRe
 
     public OktaService oktaService;
 
-    OkHttpClient client = new OkHttpClient().newBuilder().build();
-    // TODO delete logger
-    private static final Logger LOG = LoggerFactory.getLogger(OktaCodec.class);
-
     @Inject
-    public OktaResource(OktaService oktaService, MessageInputFactory messageInputFactory) throws IOException {
+    public OktaResource(OktaService oktaService, MessageInputFactory messageInputFactory) {
         super(messageInputFactory.getAvailableInputs());
         this.oktaService = oktaService;
     }
@@ -43,24 +34,9 @@ public class OktaResource extends AbstractInputsResource implements PluginRestRe
     @ApiOperation(value = "Pull Okta System Logs", response = okhttp3.Response.class)
     public OktaResponse syslogs() throws Exception {
         // TODO add apiparam for hardcoded values
-        String url = "https://company.okta.com/api/v1/logs";
+        String domain = "https://company.okta.com/api/v1/logs";
         String apiKey = "SSWS ";
-        LOG.info(url);
-        LOG.info(apiKey);
 
-        // TODO move method into OktaService.getSystemLogs
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                                                .build();
-        Request request = new Request.Builder()
-                .url(url)
-                .method("GET", null)
-                .addHeader("Accept", "application/json")
-                .addHeader("Content-Type", "application/json")
-                .addHeader("Authorization", apiKey)
-                .build();
-
-        okhttp3.Response response = client.newCall(request).execute();
-        OktaResponse oktaResponse = OktaResponse.create(response.body().string());
-        return oktaResponse;
+        return oktaService.getSystemLogs(domain, apiKey);
     }
 }
