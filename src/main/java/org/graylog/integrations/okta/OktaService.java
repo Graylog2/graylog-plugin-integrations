81c7cc76640721d1eec85f7345791b8dce41355e
Claudia Ortiz <claudia@graylog.com>
2020-02-26 12:05:23 -0600

    Add getSystemLogs method

--- src/main/java/org/graylog/integrations/okta/OktaService.java
+++ src/main/java/org/graylog/integrations/okta/OktaService.java
@@ -2,11 +2,15 @@ package org.graylog.integrations.okta;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import org.graylog2.inputs.InputService;
 import org.graylog2.plugin.system.NodeId;
 import org.graylog2.shared.inputs.MessageInputFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class OktaService {
 
@@ -25,5 +29,20 @@ public class OktaService {
         this.messageInputFactory = messageInputFactory;
         this.nodeId = nodeId;
         this.objectMapper = objectMapper;
+    }
+
+    public OktaResponse getSystemLogs(String url, String apiKey) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        Request request = new Request.Builder()
+                .url(url)
+                .method("GET", null)
+                .addHeader("Accept", "application/json")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", apiKey)
+                .build();
+
+        okhttp3.Response response = client.newCall(request).execute();
+        OktaResponse oktaResponse = OktaResponse.create(response.body().string());
+        return oktaResponse;
     }
 }
