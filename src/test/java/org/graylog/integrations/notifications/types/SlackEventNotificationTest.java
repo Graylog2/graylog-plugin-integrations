26744aaf6b371ba228f3c21c8131f862f15d72f5
Priya Rammohan <varpri@gmail.com>
2020-10-14 17:47:45 -0500

    clean up code

--- src/test/java/org/graylog/integrations/notifications/types/SlackEventNotificationTest.java
+++ src/test/java/org/graylog/integrations/notifications/types/SlackEventNotificationTest.java
@@ -1,35 +1,53 @@
 package org.graylog.integrations.notifications.types;
 
 import com.floreysoft.jmte.Engine;
+import com.github.joschi.jadconfig.util.Duration;
 import com.google.common.collect.ImmutableList;
+import okhttp3.OkHttpClient;
+import okhttp3.mockwebserver.MockWebServer;
 import org.bson.types.ObjectId;
 import org.graylog.events.notifications.*;
 import org.graylog.events.notifications.types.HTTPEventNotificationConfig;
+import org.graylog2.notifications.Notification;
+import org.graylog2.notifications.NotificationImpl;
+import org.graylog2.notifications.NotificationService;
 import org.graylog2.plugin.Message;
 import org.graylog2.plugin.MessageSummary;
 import org.graylog2.plugin.streams.Stream;
+import org.graylog2.plugin.system.NodeId;
 import org.graylog2.shared.bindings.providers.ObjectMapperProvider;
+import org.graylog2.shared.bindings.providers.OkHttpClientProvider;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.io.IOException;
+import java.net.Proxy;
+import java.net.URI;
 import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class SlackEventNotificationTest {
+@RunWith(MockitoJUnitRunner.class)
+public class SlackEventNotificationTest extends SlackClientTest {
 
     private SlackEventNotification slackEventNotification;
     private SlackEventNotificationConfig slackEventNotificationConfig;
     private EventNotificationContext eventNotificationContext;
 
+    @Mock
+    NodeId mockNodeId;
 
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
 
         slackEventNotificationConfig = new AutoValue_SlackEventNotificationConfig.Builder()
                                        .notifyChannel(true)
@@ -45,7 +63,8 @@ public class SlackEventNotificationTest {
 
         slackEventNotificationConfig.validate();
         //todo: make method `getDummyContext` public
-        eventNotificationContext = NotificationTestData.getDummyContext(getHttpNotification(), "ayirp");
+        eventNotificationContext = NotificationTestData.getDummyContext(getHttpNotification(), "ayirp").toBuilder().notificationConfig(slackEventNotificationConfig).build();
+
 
         final ImmutableList<MessageSummary> messageSummaries = ImmutableList.of(
                 new MessageSummary("graylog_1", new Message("Test message 1", "source1", new DateTime(2020, 9, 6, 17, 0, DateTimeZone.UTC))),
@@ -53,8 +72,14 @@ public class SlackEventNotificationTest {
         );
         EventNotificationService notificationCallbackService = mock(EventNotificationService.class);
         when(notificationCallbackService.getBacklogForEvent(eventNotificationContext)).thenReturn(messageSummaries);
-        slackEventNotification = new SlackEventNotification(notificationCallbackService, new ObjectMapperProvider().get(), Engine.createEngine());
 
+        NotificationService mockNotificationService = mock(NotificationService.class);
+        Notification notification = new NotificationImpl();
+        when(mockNotificationService.buildNow()).thenReturn(notification);
+
+        slackEventNotification = new SlackEventNotification(notificationCallbackService, new ObjectMapperProvider().get(),
+                Engine.createEngine(),
+                mockNotificationService,clientProvider(),mockNodeId);
 
     }
 
@@ -68,8 +93,6 @@ public class SlackEventNotificationTest {
                         .build())
                 .build();
     }
-
-
 
     @Test
     public void createSlackMessage() {
@@ -124,7 +147,8 @@ public class SlackEventNotificationTest {
     }
 
     //todo:task2
-    @Test
-    public void execute() {
+    @Test(expected = PermanentEventNotificationException.class)
+    public void execute() throws PermanentEventNotificationException {
+        slackEventNotification.execute(eventNotificationContext);
     }
 }
