869e92e23c4730050eb5135f3f6e3dfb193cfeae
Priya <varpri@gmail.com>
2020-10-19 16:03:07 -0500

    Update src/main/java/org/graylog/integrations/notifications/types/SlackClient.java
    Co-authored-by: Rob Curtis <6466251+waab76@users.noreply.github.com>

--- src/main/java/org/graylog/integrations/notifications/types/SlackClient.java
+++ src/main/java/org/graylog/integrations/notifications/types/SlackClient.java
@@ -81,64 +81,6 @@ public class SlackClient {
 		}
     }
 
-	public void send(SlackMessage message) throws SlackClientException {
-		final URL url;
-		try {
-			url = new URL(webhookUrl);
-		} catch (MalformedURLException e) {
-			throw new SlackClientException("Error while constructing webhook URL.", e);
-		}
-
-		final HttpURLConnection conn;
-		try {
-			if (!StringUtils.isEmpty(proxyURL)) {
-				final URI proxyUri = new URI(proxyURL);
-				InetSocketAddress sockAddress = new InetSocketAddress(proxyUri.getHost(), proxyUri.getPort());
-				final Proxy proxy = new Proxy(Proxy.Type.HTTP, sockAddress);
-				conn = (HttpURLConnection) url.openConnection(proxy);
-			} else {
-				conn = (HttpURLConnection) url.openConnection();
-			}
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type", "application/json");
-		} catch (URISyntaxException | IOException e) {
-			throw new SlackClientException("Could not open connection to Slack API", e);
-		}
-
-		try (final Writer writer = new OutputStreamWriter(conn.getOutputStream(), StandardCharsets.UTF_8)) {
-			String json = message.getJsonString();
-			writer.write(json);
-			writer.flush();
-
-			final int responseCode = conn.getResponseCode();
-			if (responseCode != 200) {
-				if(LOG.isDebugEnabled()){
-					try (final InputStream responseStream = conn.getInputStream()) {
-						final byte[] responseBytes = IOUtils.toByteArray(responseStream);
-						final String response = new String(responseBytes, StandardCharsets.UTF_8);
-						LOG.debug("Received HTTP response body:\n{}", response);
-					}
-				}
-				throw new SlackClientException("Unexpected HTTP response status " + responseCode);
-			}
-		} catch (IOException e) {
-			throw new SlackClientException("Could not POST to Slack API", e);
-		}
-
-		try (final InputStream responseStream = conn.getInputStream()) {
-			final byte[] responseBytes = IOUtils.toByteArray(responseStream);
-
-			final String response = new String(responseBytes, StandardCharsets.UTF_8);
-			if (response.equals("ok")) {
-				LOG.debug("Successfully sent message to Slack.");
-			} else {
-				LOG.warn("Message couldn't be successfully sent. Response was: {}", response);
-			}
-		} catch (IOException e) {
-			throw new SlackClientException("Could not read response body from Slack API", e);
-		}
-	}
 
 
 	public static class SlackClientException extends Exception {
