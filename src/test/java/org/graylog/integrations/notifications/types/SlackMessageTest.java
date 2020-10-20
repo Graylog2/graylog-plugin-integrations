104721d2d4d8daeabade8dc6b78fde91f3bd2509
Priya Rammohan <varpri@gmail.com>
2020-10-20 15:06:38 -0500

    optimize imports

--- src/test/java/org/graylog/integrations/notifications/types/SlackMessageTest.java
+++ src/test/java/org/graylog/integrations/notifications/types/SlackMessageTest.java
@@ -1,7 +1,5 @@
 package org.graylog.integrations.notifications.types;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 
 import java.io.IOException;
