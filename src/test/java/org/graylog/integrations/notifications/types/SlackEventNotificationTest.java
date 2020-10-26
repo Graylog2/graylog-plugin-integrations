b745d8a18023ca70fbec86b30ca1b10e4670639e
Priya <varpri@gmail.com>
2020-10-26 13:13:17 -0500

    Update src/test/java/org/graylog/integrations/notifications/types/SlackEventNotificationTest.java
    Co-authored-by: Rob Curtis <6466251+waab76@users.noreply.github.com>

--- src/test/java/org/graylog/integrations/notifications/types/SlackEventNotificationTest.java
+++ src/test/java/org/graylog/integrations/notifications/types/SlackEventNotificationTest.java
@@ -30,7 +30,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class SlackEventNotificationTest extends SlackPluginTestFixture {
+public class SlackEventNotificationTest {
 
     @Mock 
     EventNotificationService mockEventNotificationService;
