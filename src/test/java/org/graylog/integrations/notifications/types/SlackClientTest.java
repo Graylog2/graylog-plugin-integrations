5b90c6f16aac0e34e030a66ec6d4dd35b172aa72
Priya Rammohan <varpri@gmail.com>
2020-10-27 17:13:39 -0500

    added more unit test methods

--- src/test/java/org/graylog/integrations/notifications/types/SlackClientTest.java
+++ src/test/java/org/graylog/integrations/notifications/types/SlackClientTest.java
@@ -8,6 +8,8 @@ import okhttp3.Request;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 import okhttp3.mockwebserver.MockWebServer;
+import org.graylog.events.notifications.PermanentEventNotificationException;
+import org.graylog.events.notifications.TemporaryEventNotificationException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -38,13 +40,37 @@ public class SlackClientTest {
     }
 
 
-
     @Test
     public void send_is_successful() throws Exception {
         SlackClient slackClient = new SlackClient(mockHttpClient);
         SlackMessage message = new SlackMessage("Henry Hühnchen(little chicken)");
         slackClient.send(message,"http://url.com");
     }
+
+    @Test(expected = TemporaryEventNotificationException.class)
+    public void send_fails_with_io_exception() throws Exception {
+
+        final OkHttpClient okHttpClient = mock(OkHttpClient.class);
+        final Call remoteCall = mock(Call.class);
+        when(remoteCall.execute()).thenThrow(new IOException("Request timeout"));
+        when(okHttpClient.newCall(any())).thenReturn(remoteCall);
+
+
+        SlackClient slackClient = new SlackClient(okHttpClient);
+        SlackMessage message = new SlackMessage("Henry Hühnchen(little chicken)");
+        slackClient.send(message,"http://url.com");
+    }
+
+    @Test(expected = PermanentEventNotificationException.class)
+    public void send_fails_with_badHook_url() throws Exception {
+
+        final OkHttpClient okHttpClient = getMockHttpClient("{\"key\": \"val\"}",402);
+        SlackClient slackClient = new SlackClient(okHttpClient);
+        SlackMessage message = new SlackMessage("Henry Hühnchen(little chicken)");
+        slackClient.send(message,"http://url.com");
+    }
+
+
 
     private static OkHttpClient getMockHttpClient(final String serializedBody,int httpCode) throws IOException {
         final OkHttpClient okHttpClient = mock(OkHttpClient.class);
