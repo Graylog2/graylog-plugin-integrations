4da3648a7d818effd26a116f7a0e3301132be69f
Priya Rammohan <varpri@gmail.com>
2020-11-04 06:45:23 -0600

    linter fix

--- src/web/event-notifications/event-notification-details/SlackNotificationDetails.jsx
+++ src/web/event-notifications/event-notification-details/SlackNotificationDetails.jsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-
-import {ReadOnlyFormGroup} from 'components/common';
+import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
-import {Well} from 'components/graylog';
+
+import { ReadOnlyFormGroup } from 'components/common';
+import { Well } from 'components/graylog';
 
 const NewExampleWell = styled(Well)(({ theme }) => css`
   margin-bottom: 5px;
@@ -12,25 +13,26 @@ const NewExampleWell = styled(Well)(({ theme }) => css`
   word-wrap: break-word;
 `);
 
-
-
 const SlackNotificationDetails = ({ notification }) => (
   <>
     <ReadOnlyFormGroup label="Webhook URL" value={notification.config.webhook_url} />
     <ReadOnlyFormGroup label="Channel" value={notification.config.channel} />
     <ReadOnlyFormGroup label="Custom Message Template "
                        value={(
-                         <NewExampleWell bsSize="small" >
+                         <NewExampleWell bsSize="small">
                            {notification.config.custom_message || <em>Empty body</em>}
                          </NewExampleWell>
                        )} />
-      <ReadOnlyFormGroup label="User Name" value={notification.config.username} />
-      <ReadOnlyFormGroup label="Notify Channel" value={notification.config.notify_channel} />
-      <ReadOnlyFormGroup label="Link Names" value={notification.config.link_names} />
-      <ReadOnlyFormGroup label="Icon URL" value={notification.config.icon_url} />
-      <ReadOnlyFormGroup label="Icon Emoji" value={notification.config.icon_emoji} />
+    <ReadOnlyFormGroup label="User Name" value={notification.config.username} />
+    <ReadOnlyFormGroup label="Notify Channel" value={notification.config.notify_channel} />
+    <ReadOnlyFormGroup label="Link Names" value={notification.config.link_names} />
+    <ReadOnlyFormGroup label="Icon URL" value={notification.config.icon_url} />
+    <ReadOnlyFormGroup label="Icon Emoji" value={notification.config.icon_emoji} />
   </>
 );
 
+SlackNotificationDetails.propTypes = {
+  notification: PropTypes.object.isRequired,
+};
 
 export default SlackNotificationDetails;
